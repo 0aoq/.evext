6bbd9cd641d4e90bac6d9cf32c0b64f9269aa2f3
0aoq <a0oq@outlook.com>
2022-02-18 21:36:29 -0500

    Scripts

--- scripts/compile.ps1
+++ scripts/compile.ps1
@@ -3,7 +3,7 @@ Write-Output -------------------------------------------------------------------
 Write-Output '[?] Compiling' 
 tsc # Compile the typescript files
 Write-Output '[?] Obfuscating'
-# terser package/dist/evext.js -o test/evext.js # Obfuscate the javascript file using terser
+terser package/dist/evext.js -o package/dist/evext.js # Obfuscate the javascript file using terser
 Write-Output '[?] Building'
 pkg package/dist/evext.js --out-path package/build --compress GZip --debug # Build the package using vercel/pkg
 Write-Output '[!] Done'
